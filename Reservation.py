a412c694ad494ad18e289eaa85686c002bf7af47
Deviljushiro <jean.miquel@live.fr>
2015-11-13 22:07:59 +0100

    Classe Reservation

---
+++ Reservation.py
@@ -0,0 +1,22 @@
+import Jeu
+import Adherent
+
+class Reservation:
+
+  def __init__(self, reserv_id = None, Jeu, Adherent, datePrevue = "", dureeEmpruntVoulue=0):
+    self.reserv_id = reserv_id
+    seld.datePrevue = datePrevue
+    self.dureeEmpruntVoulue = dureeEmpruntVoulue
+    
+  def get_reserv_id(self):
+    return self.reserv_id
+    
+  def get_datePrevue(self):
+    return self.datePrevue
+    
+  def get_dureeEmpruntVoulue(self):
+    return self.dureeEmpruntVoulue
+    
+  def reserver(j= Jeu, a= Adherent):
+    return new Reservation(self,?,j,a,?,?)    #need update
+    
