af3d9690d37432f9468413f84472ce3df33a30ee
Deviljushiro <jean.miquel@live.fr>
2015-11-15 09:55:29 +0100

    Update Connexion.py

--- Connexion.py
+++ Connexion.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 import EnsUtilisateurs
-
-from Session import Session
-
 class Connexion:
         def __init__(self,username,password):
                 self.username = username
