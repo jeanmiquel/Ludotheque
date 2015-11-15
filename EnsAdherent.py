55998b13c032d8e67638d717e6170c5d0ada49a2
Deviljushiro <jean.miquel@live.fr>
2015-11-15 10:07:13 +0100

    Update EnsAdherent.py

--- EnsAdherent.py
+++ EnsAdherent.py
@@ -10,8 +10,11 @@ cur = conn.cursor()
 def createTable():
         cur.execute("""CREATE TABLE IF NOT EXISTS EnsUtilisateurs(
                         user_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                        username STRING,
-                        password STRING,
+                        name TEXT,
+                        pseudo TEXT,
+                        password TEXT,
+                        numTel TEXT,
+                        adresse TEXT
                         abonnementValide BOOLEAN,
                         empruntEnCours BOOLEAN,
                         reservationEnCours BOOLEAN,
@@ -23,66 +26,71 @@ def destroyTable():
         cur.execute("""DROP TABLE EnsUtilisateurs""")
         conn.commit()
 
-def has_username(username):
-        cur.execute("""SELECT user_id FROM EnsUtilisateurs WHERE username = ?""",(username,))
+def has_pseudo(pseudo):
+        cur.execute("""SELECT user_id FROM EnsAdherent WHERE pseudo = ?""",(pseudo,))
+        result=cur.fetchone()
+        return result != None
+        
+def has_name(name):
+        cur.execute("""SELECT user_id FROM EnsAdherent WHERE name = ?""",(name,))
         result=cur.fetchone()
         return result != None
 
 
-def is_password(password,User):
-        cur.execute("""SELECT password FROM EnsUtilisateurs WHERE user_id = ?""",(User.get_user_id(),))
+def is_password(password,Adherent):
+        cur.execute("""SELECT password FROM EnsAdherent WHERE user_id = ?""",(Adherent.get_user_id(),))
         result=cur.fetchone()
         return str(result[0]) == password
 
-def get_nombre_utilisateurs():
-        cur.execute("""SELECT COUNT(User_id) FROM EnsUtilisateurs""")
+def get_nombre_adherents():
+        cur.execute("""SELECT COUNT(user_id) FROM EnsAdherent""")
         result=cur.fetchone()
         return result[0]
 
-def get_user(user_id=None,username=None):
-        if (user_id != None): cur.execute("""SELECT * FROM EnsUtilisateurs WHERE user_id = ?""",(user_id,))
-        if (username != None): cur.execute("""SELECT * FROM EnsUtilisateurs WHERE username = ?""",(username,))
+def get_user(user_id=None,name=None):
+        if (user_id != None): cur.execute("""SELECT * FROM EnsAdherent WHERE user_id = ?""",(user_id,))
+        if (name != None): cur.execute("""SELECT * FROM EnsAdherent WHERE username = ?""",(name,))
         # Return User
         try:
                 result= cur.fetchone()
-                return Utilisateur(result[0],result[1],result[2],result[3],result[4],result[5],result[6])
+                return Adherent(result[0],result[1],result[2],result[3],result[4],result[5],result[6])
         except:
                 print "ID non valide"
         
 
 
-def delete_user(User):
+def delete_user(Adherent):
         try:
-                cur.execute("""DELETE FROM EnsUtilisateurs WHERE user_id = ?""",(User.get_user_id(),))
+                cur.execute("""DELETE FROM EnsAdherent WHERE user_id = ?""",(Adherent.get_user_id(),))
                 conn.commit()
         except: 
                 print "Erreur lors de la suppression !"
-def user_to_table(User):
+def user_to_table(Adherent):
         # User -> List
-        UserTable=(User.get_user_id(),User.get_username(),User.get_password(),User.get_abonnementValide(),User.get_empruntEnCours(),User.get_reservationEnCours(),User.get_nbRetard())
-        return UserTable
+        AdherentTable=(Adherent.get_user_id(),Adherent.get_name(),Adherent.get_password(),Adherent.get_pseudo(),Adherent.get_abonnementValide(),Adherent.get_empruntEnCours(),Adherent.get_reservationEnCours(),Adherent.get_nbRetard())
+        return AdherentTable
 
-def rechercher(username): # RAJOUTER PLUSIEURS RESULTATS :: fetchall()
-        cur.execute("""SELECT * FROM EnsUtilisateurs WHERE username LIKE ?""",(username,))
+def rechercher(name): # RAJOUTER PLUSIEURS RESULTATS :: fetchall()
+        cur.execute("""SELECT * FROM EnsAdherent WHERE name LIKE ?""",(name,))
         rows = cur.fetchall()
         return rows
 
-def insert(User):
+def insert(Adherent):
         # VERIFIER SI USERNAME EXISTES PAS DEJA
-        if not(has_username(User.get_username())):
+        if not(has_name(Adherent.get_name())):
                 try:	
-                        cur.execute("""INSERT INTO EnsUtilisateurs(user_id,username,password,abonnementValide,empruntEnCours,reservationEnCours,nbRetard) 
-                                       VALUES (?,?,?,?,?,?,?)""",user_to_table(User))
+                        cur.execute("""INSERT INTO EnsAdherent(user_id,username,pseudo,password,abonnementValide,empruntEnCours,reservationEnCours,nbRetard) 
+                                       VALUES (?,?,?,?,?,?,?)""",user_to_table(Adherent))
                         conn.commit()
-                        print("Utilisateur ajoute avec succes !")
+                        print("Adherent ajoute avec succes !")
                 except:
-                        print("Erreur lors de l'ajout de l'utilisateur")
+                        print("Erreur lors de l'ajout de l'adherent")
         else:
-                print("Erreur: Utilisateur deja existant")
+                print("Erreur: Adherent deja existant")
                 
 
 def printAll():
-        cur.execute("""SELECT * FROM EnsUtilisateurs""")
+        cur.execute("""SELECT * FROM EnsAdherent""")
         rows = cur.fetchall()
         return rows
         #for row in rows:
