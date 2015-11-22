cc98902d1577d7992b2956e28da4f81019d8de43
Deviljushiro <jean.miquel@live.fr>
2015-11-22 19:46:51 +0100

    Update AdherentClement.py

--- AdherentClement.py
+++ AdherentClement.py
@@ -1,5 +1,28 @@
 from datetime import datetime
 
+CREATE TABLE IF NOT EXISTS `Adherent` (
+`idAdh` str(6) NOT NULL,
+`nomAdh` varchar(25) NOT NULL,
+`prenomAdh` varchar(25) NOT NULL,
+`dateNaissance` date NOT NULL,
+`adresse` varchar(30) NOT NULL,
+`codePostal` varchar(10) NOT NULL,
+`ville` varchar(10) NOT NULL,
+`numTel` int(10) NOT NULL,
+`pseudo` varchar(20) NOT NULL,
+`password` varchar(26) NOT NULL,
+`mail` varchar(50) NOT NULL,
+`estAdmin` tinyint(1) NOT NULL,
+`dateAbonnement` date NOT NULL,
+`nbreRetards` int(3) NOT NULL,
+`nbreJourRetards` int(3) NOT NULL,
+`nbreReservAnnulees` int(3) NOT NULL,
+`idEmprunt` int(11) NOT NULL,
+`idReserv` int(11) NOT NULL,
+PRIMARY KEY (`idAdherent`)
+)
+
+
 class Adherent :
 
     def __init__(self, nomAdh : str, pseudoAdh : str, password : str, numTel : str, mail : str, adresse : str, dataBase = BD):
@@ -7,7 +30,7 @@ class Adherent :
       self.Table = "Adherent"
       self.idAdherent = str(mail)
       self.cursor.execute("""INSERT INTO Adherent(
-      idAdherent, nomAdh, pseudoAdh, password, numTel, mail, adresse, dateAbonnement, empruntEnCours, reservEnCours, nbreRetards, nbreJoursRetards, estAdmin, NbreReservAnnulees)
+      idAdherent, nomAdh, pseudoAdh, password, numTel, mail, adresse, dateAbonnement, empruntEnCours, reservEnCours, nbreRetards, nbreJoursRetards, estAdmin, nbreReservAnnulees)
       VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
       (self.idAdherent, nomAdh, pseudoAdh, password, numTel, mail, adresse, "", None, None, 0, 0, False, 0))
 
