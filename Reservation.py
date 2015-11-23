dcbf1c7213fb7a93da5c1fa5bfedc6c91a5a5d52
Deviljushiro <jean.miquel@live.fr>
2015-11-23 18:00:07 +0100

    Update Reservation.py

--- Reservation.py
+++ Reservation.py
@@ -1,22 +1,62 @@
 import Jeu
 import Adherent
 
+CREATE TABLE IF NOT EXISTS Reservation 
+  (idReserv str(6) NOT NULL, 
+  idAdh str(6) NOT NULL, 
+  idJeu str(6) NOT NULL, 
+  idExt str(6) NOT NULL, 
+  dateReserv date NOT NULL, 
+  dureeEmpruntPrevue int(3) NOT NULL, 
+  KEY fk_idExtR(idExtension), 
+  KEY fk_idJeuR (idJeu),
+  KEY fk_idAdhR (idAdherent))
+
+
 class Reservation:
 
-  def __init__(self, reserv_id = None, Jeu, Adherent, datePrevue = "", dureeEmpruntVoulue=0):
-    self.reserv_id = reserv_id
-    seld.datePrevue = datePrevue
-    self.dureeEmpruntVoulue = dureeEmpruntVoulue
-    
-  def get_reserv_id(self):
-    return self.reserv_id
-    
-  def get_datePrevue(self):
-    return self.datePrevue
-    
-  def get_dureeEmpruntVoulue(self):
-    return self.dureeEmpruntVoulue
-    
-  def reserver(j= Jeu, a= Adherent):
-    return new Reservation(self,?,j,a,?,?)    #need update
-    
+  def __init__(self, dataBase = BD):
+    		self.cursor = dataBase.cursor()
+    		self.Table = "Reservation"
+
+    		self.cursor.execute("""INSERT INTO Reservation(
+		idReserv, idJeu, idAdh, idExt, dateReserv, dureeEmpruntPrevue)
+		VALUES(?, ?, ?, ?, ?, ?)""",
+    (self.idReserv, self.idJeu, self.idAdh, self.idExt, date, 14)) #14 jours
+
+	#setters ?
+	def setIdReserv(self, idReserv : str) :       
+    		self.cursor.execute("""UPDATE Reservation SET idReserv = ? WHERE idReserv = ?""",
+                        	(idReserv, self.idReserv))
+    		self.idReserv = idReserv
+    		return self
+   	 
+	def setDateReserv(self, dateReserv : date) :    
+    		self.cursor.execute("""UPDATE Reservation SET dateReserv = ? WHERE idReserv = ?""",
+                        	(dateReserv, self.idReserv))
+    		return self
+
+	def setDureeEmpruntPrevue(self, dureeEmpruntPrevue : int) : 	
+    		self.cursor.execute("""UPDATE Reservation SET dureeEmpruntPrevue = ? WHERE idReserv = ?""",
+                        	(dureeEmpruntPrevue, self.idReserv))
+    		return self
+
+
+	#getters ?
+	
+	def getIdReserv(self):
+	  return self.idReserv
+	  
+	def getDateReserv(self):
+	  return self.dateReserv
+	  
+	def getDureeEmpruntPrevue(self):
+	  return self.dureeEmpruntPrevue
+	  
+	#Fonctions usuelles:
+	
+	def annulerReserv(self):
+	  self.cursor.execute("""DELETE FROM Reservation Where idReserv = ?""",
+	                    (self.idReserv))
+	 
+	
