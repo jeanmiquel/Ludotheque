ba53cf4dcf07a3c57b48ea40ee06f29efe856aac
Deviljushiro <jean.miquel@live.fr>
2015-11-27 13:12:39 +0100

    Update Reservation.py

--- Reservation.py
+++ Reservation.py
@@ -10,14 +10,14 @@ conn.commit()
 cur = conn.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS Reservation 
-  (idReservation str(6) NOT NULL, 
-  idAdherent str(6) NOT NULL, 
-  idJeu str(6) NOT NULL, 
-  idExtension str(6) NOT NULL, 
+  (idReservation int(6) NOT NULL, 
+  idAdherent int(6) NOT NULL, 
+  idJeu int(6) NOT NULL, 
+  idExtension int(6) NOT NULL, 
   dateReservation date NOT NULL, 
   dureeEmpruntPrevue int(3) NOT NULL, 
   PRIMARY KEY (idReservation),
-  FOREIGN KEY(idExtension), 
+  FOREIGN KEY (idExtension), 
   FOREIGN KEY (idJeu),
   FOREIGN KEY (idAdherent))""")
 conn.commit()
@@ -31,23 +31,23 @@ class Reservation:
     		self.cursor.execute("""INSERT INTO Reservation(
 		idReservation, idJeu, idAdherent, idExtension, dateReservation, dureeEmpruntPrevue)
 		VALUES(?, ?, ?, ?, ?, ?)""",
-    (self.idReservation, self.idJeu, self.idAdherent, self.idExtension, date, 14)) #14 jours
+    		(self.idReservation, self.idJeu, self.idAdherent, self.idExtension, datetime.now(), 7)) #7 jours d'emprunts
 
 	#setters ?
-	def setIdReserv(self, idReserv : str) :       
+	def setIdReserv(self, idReservation) :       
     		self.cursor.execute("""UPDATE Reservation SET idReservation = ? WHERE idReservation = ?""",
                         	(idReservation, self.idReservation))
     		self.idReserv = idReservation
     		return self
    	 
-	def setDateReserv(self, dateReservation : date) :    
+	def setDateReserv(self, dateReservation) :    
     		self.cursor.execute("""UPDATE Reservation SET dateReservation = ? WHERE idReservation = ?""",
                         	(dateReservation, self.idReservation))
     		return self
 
-	def setDureeEmpruntPrevue(self, dureeEmpruntPrevue : int) : 	
+	def setDureeEmpruntPrevue(self, dureeEmpruntPrevue) : 	
     		self.cursor.execute("""UPDATE Reservation SET dureeEmpruntPrevue = ? WHERE idReservation = ?""",
-                        	(dureeEmpruntPrevue, self.idReserv))
+                        	(dureeEmpruntPrevue, self.idReservation))
     		return self
 
 
@@ -57,8 +57,7 @@ class Reservation:
 	  return self.idReservation
 	  
 	def getDateReserv(self):
-          self.cursor.execute("""SELECT """,
-                        	(self.idReserv))
+          self.cursor.execute("""SELECT """, (self.idReservation))
 	  return self.dateReservation
 	  
 	def getDureeEmpruntPrevue(self):
