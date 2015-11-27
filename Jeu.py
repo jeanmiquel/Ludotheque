588e0606a853b85bdbdc6f0d0f61780497a3a8f3
Deviljushiro <jean.miquel@live.fr>
2015-11-27 15:07:33 +0100

    Update Jeu.py

--- Jeu.py
+++ Jeu.py
@@ -1,9 +1,9 @@
 import sqlite3
 import datetime
-from Extension import Extension
-from Adherent import Adherent
+import Extension
 
-conn = sqlite3.connect("C:\Users\Jean\Desktop\LUDOTHEQUE\Ludotheque.db")
+
+conn = sqlite3.connect("P:\Ludotheque-master\Ludotheque.db")
 conn.execute('pragma foreign_keys = on')
 conn.commit()
 cur = conn.cursor()
@@ -12,126 +12,131 @@ cur.execute("""CREATE TABLE IF NOT EXISTS `Jeu` (
 `idJeu` int(6) NOT NULL,
 `nomJeu` varchar(50) NOT NULL,
 `anneeJeu` int(4) NOT NULL,
+`ageJeu` int(2) NOT NULL,
 `nbJoueurJeu` varchar(5) NOT NULL,
 `quantiteJeu` int(3) NOT NULL,
 `editeurJeu` varchar(20) NOT NULL,
 `estEmpruntableJeu` tinyint(1) NOT NULL,
 `synopsisJeu` varchar(200) NOT NULL,
-PRIMARY KEY (`idJeu`)
-)""")
+PRIMARY KEY (`idJeu`))""")
+conn.commit()
 
 
 class Jeu :
     
-	def __init__(self, nomJeu, anneeJeu, dataBase = conn):
-    		self.cursor = dataBase.cursor()
-    		self.Table = "Jeu"
-    		self.idJeu = str(str(nomJeu) + str(anneeJeu))
+        def __init__(self, nomJeu, anneeJeu, ageJeu):
+                cur.execute("""INSERT INTO Jeu(
+                idJeu, nomJeu, anneeJeu, nbJoueurJeu, ageJeu,
+                quantiteJeu, editeurJeu, estEmpruntableJeu, synopsisJeu)
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                                (1000, nomJeu, anneeJeu,
+                                " ", 0, ageJeu, " ", False, " "))
 
-    		self.cursor.execute("""INSERT INTO Jeu(
-		idJeu, nomJeu, anneeJeu, nbJoueurJeu,
-		quantiteJeu, editeurJeu, estEmpruntableJeu, synopsisJeu)
-		VALUES(?, ?, ?, ?, ?, ?, ?, ?)""",
-                        	(self.idJeu, nomJeu, anneeJeu,
-                         	" ", 0, " ", False, " "))
+        #setters ?
+        def setIdJeu(self, idJeu) :            
+                cur.execute("""UPDATE Jeu SET idJeu = ? WHERE idJeu = ?""",
+                                (idJeu, idJeu))
+                idJeu = idJeu
+                conn.commit()
+         
+        def setNomJeu(self, idJeu, nomJeu) :          
+                anneeJeu = self.getAnneeJeu()
+                cur.execute("""UPDATE Jeu SET nomJeu = ? WHERE idJeu = ?""",
+                                (nomJeu, idJeu))
+                conn.commit()
 
-	#setters ?
-	def setIdJeu(self, idJeu : str) :       	#On modifie l'id du jeu, dans l'objet et dans la BD
-    		self.cursor.execute("""UPDATE Jeu SET idJeu = ? WHERE idJeu = ?""",
-                        	(idJeu, self.idJeu))
-    		self.idJeu = idJeu
-    		return self
-   	 
-	def setNomJeu(self, nomJeu : str) :     	#On modifie le nom du jeu dans la DB et donc son id
-    		anneeJeu = self.getAnneeJeu()
-    		self.cursor.execute("""UPDATE Jeu SET nomJeu = ? WHERE idJeu = ?""",
-                        	(nomJeu, self.idJeu))
-    		self.setIdJeu(str(str(nomJeu) + str(anneeJeu)))
-    		return self
+        def setAnneeJeu(self, idJeu,anneeJeu) :
+                cur.execute("""UPDATE Jeu SET anneeJeu = ? WHERE idJeu = ?""",
+                                (anneeJeu, idJeu))
+                conn.commit()
 
-	def setAnneeJeu(self, anneeJeu : int) : 	#On modifie la date du jeu, et donc son id
-    		nomJeu = self.getNomJeu()
-    		self.cursor.execute("""UPDATE Jeu SET anneeJeu = ? WHERE idJeu = ?""",
-                        	(anneeJeu, self.idJeu))
-    		self.setIdJeu(str(str(nomJeu) + str(anneeJeu)))
-    		return self
+        def setAgeJeu(self, idJeu, ageJeu):
+                cur.execute("""UPDATE Jeu SET AgeJEu =? WHERE idJeu =?""",
+                                    (ageJeu, idJeu))
+                conn.commit()
+                
 
-	def setNbJoueurJeu(self, nbJoueurJeu : str):
-    		self.cursor.execute(""" UPDATE Jeu SET nbJoueurJeu = ? WHERE idJeu = ?""",(nbJoueurJeu,self.idJeu))
-    		return self
+        def setNbJoueurJeu(self, idJeu,nbJoueurJeu):
+                cur.execute(""" UPDATE Jeu SET nbJoueurJeu = ? WHERE idJeu = ?""",(nbJoueurJeu,idJeu))
+                conn.commit()
 
-	def setQuantiteJeu(self, quantiteJeu : int) :
-    		self.cursor.execute("""UPDATE Jeu SET quantiteJeu = ? WHERE idJeu = ?""",(quantiteJeu, self.idJeu))
-    		return self
+        def setQuantiteJeu(self, idJeu,quantiteJeu) :
+                cur.execute("""UPDATE Jeu SET quantiteJeu = ? WHERE idJeu = ?""",(quantiteJeu, idJeu))
+                conn.commit()
 
-	def setEditeurJeu(self, editeurJeu : str) :
-    		self.cursor.execute("""UPDATE Jeu SET editeurJeu = ? WHERE idJeu = ?""",(editeurJeu, self.idJeu))
-    		return self
+        def setEditeurJeu(self, idJeu,editeurJeu) :
+                cur.execute("""UPDATE Jeu SET editeurJeu = ? WHERE idJeu = ?""",(editeurJeu, idJeu))
+                conn.commit()
 
-	def setEmpruntable(self, booleen : bool):
-    		self.cursor.execute("""UPDATE Jeu SET estEmpruntableJeu = ? WHERE idJeu = ?""",(booleen, self.idJeu))
-    		return self
+        def setEmpruntable(self,idJeu, booleen):
+                cur.execute("""UPDATE Jeu SET estEmpruntableJeu = ? WHERE idJeu = ?""",(booleen, idJeu))
+                conn.commit()
 
-	def setSynopsisJeu(self, synopsisJeu : str):
-    		self.cursor.execute("""UPDATE Jeu SET synopsisJeu = ? WHERE idJeu = ?""",(synopsisJeu, self.idJeu))
-    		return self
+        def setSynopsisJeu(self,idJeu, synopsisJeu):
+                cur.execute("""UPDATE Jeu SET synopsisJeu = ? WHERE idJeu = ?""",(synopsisJeu, idJeu))
+                conn.commit()
  
 
 
-	#getter
-	
-	def getInfoJeu(self) :                  	#Saisir tout d'un coup
-    		"""0 Id, 1 Nom, 2 AnnÃ©e, 3 NbJoueur, 4 QuantitÃ©, 5 Editeur, 6 Empruntable, 7 Synospis"""
-    		self.cursor.execute("""SELECT
-		idJeu, nomJeu, anneeJeu, nbJoueurJeu,
-		quantiteJeu, editeurJeu, estEmpruntableJeu, synopsisJeu
-		FROM Jeu Where idJeu = ?""",(self.idJeu))
-    		return self.cursor.fetchone()
+        #getter
+        
+        def getInfoJeu(self,idJeu) :                         
+                """0 Id, 1 Nom, 2 Annee, 3 NbJoueur, 4 Quantite, 5 Editeur, 6 Empruntable, 7 Synospis"""
+                cur.execute("""SELECT
+                idJeu, nomJeu, anneeJeu, nbJoueurJeu,
+                quantiteJeu, editeurJeu, estEmpruntableJeu, synopsisJeu
+                FROM Jeu Where idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
     
-	def getIdJeu(self) : 
-		return str(self.idJeu) #L'id est accessible dans les attributs
+        def getIdJeu(self,nomJeu) : 
+                cur.execute("""SELECT idJeu FROM Jeu WHERE nomJeu = ?""",(nomJeu))
+                return cur.fetchone()[0]
 
-	def getNomJeu(self) :
-                #Le nom est stockÃ© dans la BD
-                self.cursor.execute("""SELECT nomJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
+        def getNomJeu(self,idJeu) :
 
-	def getAnneeJeu(self) :         #L'annee est stokÃ© dans la DB
-    		self.cursor.execute("""SELECT anneeJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
-    	
-	def getNbJoueurJeu(self) :
-    		self.cursor.execute("""SELECT nbJoueurJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
+                cur.execute("""SELECT nomJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
 
-	def getQuantiteJeu(self):
-    		self.cursor.execute("""SELECT quantiteJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
+        def getAnneeJeu(self,idJeu) :        
+                cur.execute("""SELECT anneeJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
+        
+        def getNbJoueurJeu(self,idJeu) :
+                cur.execute("""SELECT nbJoueurJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
 
-	def getEditeurJeu(self):
-    		self.cursor.execute("""SELECT editeurJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
+        def getQuantiteJeu(self,idJeu):
+                cur.execute("""SELECT quantiteJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
 
-	def estEmpruntable(self):
-    		self.cursor.execute("""SELECT estEmpruntableJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
+        def getEditeurJeu(self,idJeu):
+                cur.execute("""SELECT editeurJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
 
-	def getSynopsisJeu(self):
-    		self.cursor.execute("""SELECT synopsisJeu FROM WHERE idJeu = ?""",(self.idJeu))
-    		return cursor.fetchone()
+        def estEmpruntable(self,idJeu):
+                cur.execute("""SELECT estEmpruntableJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
 
-	#Fonctions usuelles:
+        def getSynopsisJeu(self,idJeu):
+                cur.execute("""SELECT synopsisJeu FROM Jeu WHERE idJeu = ?""",(idJeu))
+                return cur.fetchone()[0]
 
-	def ajoutExemplaire(self):
-    		return self.setQuantiteJeu(self.getQuantiteJeu()+1)
+        #Fonctions usuelles:
 
-	def retraitExemplaire(self):
-    		return self.setQuantiteJeu(min(self.getQuantiteJeu()-1,0))
+        def ajoutExemplaire(self,idJeu):
+                self.setQuantiteJeu(self.getQuantiteJeu(idJeu)+1,idJeu)
+                conn.commit()
+                
 
-	def changeEmpruntable(self):
-    		return self.setEmpruntable(not self.estEmpruntable())
+        def retraitExemplaire(self,idJeu):
+                self.setQuantiteJeu(max(self.getQuantiteJeu(idJeu)-1,0),idJeu)
+                conn.commit()
+
+        def changeEmpruntable(self,idJeu):
+                self.setEmpruntable(not self.estEmpruntable(idJeu),idJeu)
+                conn.commit()
     
-	def supprimerJeu(self):
-    		cursor.execute("""DELETE FROM Jeu WHERE idJeu""",(self.idJeu,))
-    		return self
+        def supprimerJeu(self,idJeu):
+                cur.execute("""DELETE FROM Jeu WHERE idJeu""",(idJeu))
+                conn.commit()
 
