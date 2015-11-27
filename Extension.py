ef34b656b4f92a8d636c157697bea1a42ce12532
Deviljushiro <jean.miquel@live.fr>
2015-11-27 14:57:14 +0100

    Update Extension.py

--- Extension.py
+++ Extension.py
@@ -1,54 +1,55 @@
 import datetime
 import sqlite3
-from Jeu import Jeu
-from Adherent import Adherent
+import Jeu
 
-conn = sqlite3.connect("C:\Users\Jean\Desktop\LUDOTHEQUE\Ludotheque.db")
+
+conn = sqlite3.connect("P:\Ludotheque-master\Ludotheque.db")
 conn.execute('pragma foreign_keys = on')
 conn.commit()
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS Extension (
- idExtension int(6) NOT NULL, 
- idJeu int(6) NOT NULL, 
- nomExtension varchar(20) NOT NULL, 
- nbreTotalExtension int(3) NOT NULL
- PRIMARY KEY (idExtension),
- FOREIGN KEY (idJeu))""")
-
+cur.execute("""CREATE TABLE IF NOT EXISTS `Extension`
+    (`idExtension` int(6) NOT NULL, 
+     `idJeu` int(6) NOT NULL, 
+     `nomExtension` varchar(20) NOT NULL, 
+     `nbreTotalExtension` int(3) NOT NULL,
+     PRIMARY KEY (`idExtension`))""")
+conn.commit()
+    
 class Extension:
 
- def __init__(self, nomExtension, nbreTotalExtension, database = conn):
-      self.cursor = dataBase.cursor()
-    		self.Table = "Extension"
+     def __init__(self, nomExtension):
+                cur.execute("""INSERT INTO Extension(idExtension, idJeu, nomExtension, nbreTotalExtension)
+                VALUES(?, ?, ?, ?)""",
+                (0, 0, self.nomExtension, 1))
+                                    
 
-    		self.cursor.execute("""INSERT INTO Extension(
-		idExtension, idJeu, nomExtension, nbreTotalExtension)
-		VALUES(?, ?, ?, ?)""",
-    (self.idExtension, self.idJeu, self.nomExtension, self.nbreTotalExtension)
+###SETTERS###
     
- def setIdExtension(self, idExtension) :       
-    		self.cursor.execute("""UPDATE Extension SET idExtension = ? WHERE idExtension = ?""",
-                        	(idExtension, self.idExtension))
-    		self.idExtension = idExtension
-    		return self
+     def setIdExtension(self, idExtension):       
+            cur.execute("""UPDATE Extension SET idExtension = ? WHERE idExtension = ?""",
+                            (idExtension, self.idExtension))
+            conn.commit()
  
- def setNomExtension(self, nomExtension) :       
-    		self.cursor.execute("""UPDATE Extension SET nomExtension = ? WHERE idExtension = ?""",
-                        	(nomExtension, self.idExtension))
-    		return self
+     def setNomExtension(self,idExtension, nomExtension) :       
+            cur.execute("""UPDATE Extension SET nomExtension = ? WHERE idExtension = ?""",
+                            (nomExtension, self.idExtension))
+            conn.commit()
 
-def setNbreTotalExtension(self, nbreTotalExtension) :       
-    		self.cursor.execute("""UPDATE Extension SET nbreTotalExtension = ? WHERE idExtension = ?""",
-                        	(nbreTotalExtension, self.idExtension))
-    		return self
+     def setNbreTotalExtension(self,idExtension, nbreTotalExtension) :       
+            cur.execute("""UPDATE Extension SET nbreTotalExtension = ? WHERE idExtension = ?""",
+                            (nbreTotalExtension, self.idExtension))
+            conn.commit()
 
-def getIdExtension(self):
-		self.cursor.execute("""SELECT idExtension = ? FROM Extension WHERE idExtension = ?""",
-                        	(idExtension, self.idExtension))
-                return cursor.fetchone()
-def getNomExtension(self):
-		self.cursor.execute("""SELECT nomExtension = ? FROM Extension WHERE idExtension = ?""",
-                        	(nomExtension, self.idExtension))
-                return cursor.fetchone()
+###GETTERS###
+
+     def getIdExtension(self,idExtension):
+        cur.execute("""SELECT idExtension = ? FROM Extension WHERE idExtension = ?""",
+                            (idExtension, self.idExtension))
+        return cursor.fetchone()
+    
+     def getNomExtension(self,idExtension):
+        cur.execute("""SELECT nomExtension = ? FROM Extension WHERE idExtension = ?""",
+                            (nomExtension, self.idExtension))
+        return cursor.fetchone()
                 
