5c8a1fccbe93634892c7f70b6e3c63f50fdc5d1e
coooola <nicolas.bitan@free.fr>
2015-12-16 18:19:26 +0100

    Update Extension.py

--- Extension.py
+++ Extension.py
@@ -2,20 +2,16 @@ import datetime
 import sqlite3
 import Jeu
 
+import BDD
 
-conn = sqlite3.connect("ludotheque.db")
-conn.execute('pragma foreign_keys = on')
-conn.commit()
-cur = conn.cursor()
-
-cur.execute("""CREATE TABLE IF NOT EXISTS `Extension`
+BDD.cur.execute("""CREATE TABLE IF NOT EXISTS `Extension`
     (`idExtension` int(6) NOT NULL, 
      `idJeu` int(6) NOT NULL, 
      `nomExtension` varchar(20) NOT NULL, 
      `nbreTotalExtension` int(3) NOT NULL,
      PRIMARY KEY (`idExtension`)
      FOREIGN KEY (`idJeu`) REFERENCES Jeu(`idJeu`))""")
-conn.commit()
+BDD.conn.commit()
     
 class Extension:
 
@@ -23,63 +19,63 @@ class Extension:
 
     @staticmethod
     def setNomExtension(idExtension, nomExtension) :       
-            cur.execute("""UPDATE Extension SET nomExtension = ? WHERE idExtension = ?""",
+            BDD.cur.execute("""UPDATE Extension SET nomExtension = ? WHERE idExtension = ?""",
                             (nomExtension, idExtension))
-            conn.commit()
+            BDD.conn.commit()
     
     @staticmethod
     def setNbreTotalExtension(idExtension, nbreTotalExtension) :       
-            cur.execute("""UPDATE Extension SET nbreTotalExtension = ? WHERE idExtension = ?""",
+            BDD.cur.execute("""UPDATE Extension SET nbreTotalExtension = ? WHERE idExtension = ?""",
                             (nbreTotalExtension, idExtension))
-            conn.commit()
+            BDD.conn.commit()
 
 ###GETTERS###
 
     @staticmethod
     def getIdExtension(nomExtension):
-        cur.execute("""SELECT idExtension  FROM Extension WHERE nomExtension = ?""",
+        BDD.cur.execute("""SELECT idExtension  FROM Extension WHERE nomExtension = ?""",
                             (nomExtension,))
-        return cur.fetchone()[0]
+        return BDD.cur.fetchone()[0]
     
     @staticmethod
     def getNomExtension(idExtension):
-        cur.execute("""SELECT nomExtension FROM Extension WHERE idExtension = ?""",
+        BDD.cur.execute("""SELECT nomExtension FROM Extension WHERE idExtension = ?""",
                             (idExtension,))
-        return cur.fetchone()[0]
+        return BDD.cur.fetchone()[0]
     
     @staticmethod         
     def getNbreTotalExtension(idExtension) :
-        cur.execute("""SELECT nbreTotalExtension  FROM Extension WHERE idExtension = ?""",
+        BDD.cur.execute("""SELECT nbreTotalExtension  FROM Extension WHERE idExtension = ?""",
                             (idExtension,))
-        return cur.fetchone()[0]
+        return BDD.cur.fetchone()[0]
     
     @staticmethod    
     def getIdJeu(idExtension) :
-        cur.execute("""SELECT idJeu  FROM Extension WHERE idExtension = ?""",
+        BDD.cur.execute("""SELECT idJeu  FROM Extension WHERE idExtension = ?""",
                             (idExtension,))
-        return cur.fetchone()[0]
+        return BDD.cur.fetchone()[0]
 
 #Fonctions usuelles:
 
     @staticmethod
     def afficherTableExtension():
-        cur.execute("""SELECT * FROM Extension""")
-        return cur.fetchall()
+        BDD.cur.execute("""SELECT * FROM Extension""")
+        return BDD.cur.fetchall()
 
     @staticmethod
     def ajoutExtension(idJeu, nomExtension, nbreTotalExtension):
-        cur.execute("""SELECT MAX(idExtension) FROM Extension""")
-        f = cur.fetchone()[0]
+        BDD.cur.execute("""SELECT MAX(idExtension) FROM Extension""")
+        f = BDD.cur.fetchone()[0]
         if (f==None):
             idExtension = 1
         else:
             idExtension =f+1
-        cur.execute("""INSERT INTO Extension(idExtension, idJeu, nomExtension, nbreTotalExtension)
+        BDD.cur.execute("""INSERT INTO Extension(idExtension, idJeu, nomExtension, nbreTotalExtension)
                     VALUES(?, ?, ?, ?)""",
                     (idExtension, idJeu, nomExtension, nbreTotalExtension))
-        conn.commit()
+        BDD.conn.commit()
                     
     @staticmethod
     def supprimerExtension(idExtension):
-        cur.execute("""DELETE FROM Extension WHERE idExtension = ?""",(idExtension,))
-        conn.commit()
+        BDD.cur.execute("""DELETE FROM Extension WHERE idExtension = ?""",(idExtension,))
+        BDD.conn.commit()
