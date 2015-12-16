9298b35f9933fb662429e9938485ad26e8f16662
coooola <nicolas.bitan@free.fr>
2015-12-16 18:20:11 +0100

    Update Categorie.py

--- Categorie.py
+++ Categorie.py
@@ -1,67 +1,65 @@
 import sqlite3
 import datetime
 
-conn = sqlite3.connect("ludotheque.db")
-conn.execute('pragma foreign_keys = on')
-conn.commit()
-cur = conn.cursor()
+import BDD
 
-cur.execute("""CREATE TABLE IF NOT EXISTS `Categorie` (
+BDD.cur.execute("""CREATE TABLE IF NOT EXISTS `Categorie` (
 `idCategorie` int(6) NOT NULL,
 `nomCategorie` varchar(25) NOT NULL,
 PRIMARY KEY (`idCategorie`))""")
-conn.commit()
+BDD.conn.commit()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS `Appartient` (
+BDD.cur.execute("""CREATE TABLE IF NOT EXISTS `Appartient` (
 `idCategorie` int(6) NOT NULL,
 `idJeu` int(6) NOT NULL,
 PRIMARY KEY (`idCategorie`,`idJeu`),
 FOREIGN KEY (`idCategorie`) REFERENCES Categorie(`idCategorie`),
 FOREIGN KEY (`idJeu`) REFERENCES Jeu(`idJeu`)
 )""")
-conn.commit()
+BDD.conn.commit()
 
 class Categorie :
   
   #setters
   @staticmethod
   def setNomJeu(idCategorie, nomCategorie) :
-    cur.execute("""UPDATE Categorie SET nomCategorie = ? WHERE idCategorie = ?""",
+    BDD.cur.execute("""UPDATE Categorie SET nomCategorie = ? WHERE idCategorie = ?""",
                 (nomCategorie, idCategorie))
-    conn.commit()
+    BDD.conn.commit()
     
   #getters
-   @staticmethod
-  def getIdCategorie(nomJeu) :
-    cur.execute("""SELECT idCategorie FROM Categorie WHERE nomCategorie = ?""",(nomCategorie,))
-    return cur.fetchone()[0]
+    @staticmethod
+    def getIdCategorie(nomJeu):
+      BDD.cur.execute("""SELECT idCategorie FROM Categorie WHERE nomCategorie = ?""",(nomCategorie,))
+      return BDD.cur.fetchone()[0]
     
   @staticmethod
   def getNomCategorie(idCategorie) :
-    cur.execute("""SELECT nomCategorie FROM Categorie WHERE idCategorie = ?""",(idCategorie,))
-    return cur.fetchone()[0]
+    BDD.cur.execute("""SELECT nomCategorie FROM Categorie WHERE idCategorie = ?""",(idCategorie,))
+    return BDD.cur.fetchone()[0]
     
   #Fonctions usuelles
   
   @staticmethod
   def afficherTableCategorie():
-    cur.execute("""SELECT * FROM Categorie""")
-    return cur.fetchall()
+    BDD.cur.execute("""SELECT * FROM Categorie""")
+    return BDD.cur.fetchall()
   
   @staticmethod
   def ajoutCategorie(nomCategorie):
-    cur.execute("""SELECT MAX(idCategorie) FROM Categorie""")
-    f = cur.fetchone()[0]
+    BDD.cur.execute("""SELECT MAX(idCategorie) FROM Categorie""")
+    f = BDD.cur.fetchone()[0]
     if (f==None):
         idJeu = 1
     else:
         idJeu =f+1
-    cur.execute("""INSERT INTO Categorie(idCategorie, nomCategorie)
+    BDD.cur.execute("""INSERT INTO Categorie(idCategorie, nomCategorie)
                     VALUES(?, ?)""",(idCategorie, nomCategorie))
-    conn.commit()
+    BDD.conn.commit()
             
   @staticmethod
   def supprimerCategorie(idCategorie):
-    cur.execute("""DELETE FROM Categorie WHERE idCategorie = ?""",(idCategorie,))
-    conn.commit()
+    BDD.cur.execute("""DELETE FROM Categorie WHERE idCategorie = ?""",(idCategorie,))
+    BDD.conn.commit()
+
 
