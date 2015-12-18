8bca13635cf155a6dfe1acee2088ade0c49b42b6
Deviljushiro <jean.miquel@live.fr>
2015-12-18 15:20:32 +0100

    Update Reservation.py

--- Reservation.py
+++ Reservation.py
@@ -71,12 +71,21 @@ class Reservation:
   #Fonctions usuelles:
   
   @staticmethod
+  def aUnJeu(idReservation):
+    return(Reservation.getIdJeuReserv(idReservation) != None)
+  
+  @staticmethod
+  def aUneExtension(idReservation):
+    return(Reservation.getIdExtensionReserv(idReservation) != None)
+  
+  @staticmethod
   def getAllReservations():
     BDD.cur.execute("""SELECT * FROM Reservation""")
     return BDD.cur.fetchall()
   
+  
   @staticmethod
-  def ajoutReservation():
+  def reserverJeu(idAdherent, idJeu, dateReservation, dureeEmpruntPrevue):
     BDD.cur.execute("""SELECT MAX(idReservation) FROM Reservation""")
     f = BDD.cur.fetchone()[0]
     if (f==None):
@@ -86,16 +95,66 @@ class Reservation:
     BDD.cur.execute("""INSERT INTO Reservation(
                  idReservation, idJeu, idAdherent, idExtension, dateReservation, dureeEmpruntPrevue)
                 VALUES(?, ?, ?, ?, ?, ?)""",
-                (idReservation, idJeu, idAdherent, idExtension, datetime.now(), 7)) #7 jours d'emprunts : a faire
+                (idReservation, idJeu, idAdherent, None, dateReservation, dureeEmpruntPrevue)) 
+    BDD.conn.commit()
+    
+    @staticmethod
+  def reserverExtensionAvecJeu(idAdherent, idExtension, dateReservation, dureeEmpruntPrevue):
+    idJeuExt = Extension.getIdJeu(idExtension) #on récupère l'id du jeu correspondant à l'extension
+    BDD.cur.execute("""SELECT MAX(idReservation) FROM Reservation""")
+    f = BDD.cur.fetchone()[0]
+    if (f==None):
+      idReservation = 1
+    else:
+      idReservation =f+1
+    BDD.cur.execute("""INSERT INTO Reservation(
+                 idReservation, idJeu, idAdherent, idExtension, dateReservation, dureeEmpruntPrevue)
+                VALUES(?, ?, ?, ?, ?, ?)""",
+                (idReservation, idJeuExt, idAdherent,idExtension, dateReservation, dureeEmpruntPrevue)) 
+    BDD.conn.commit()
+    
+    @staticmethod
+  def reserverExtensionSansJeu(idAdherent, idExtension, dateReservation, dureeEmpruntPrevue):
+    BDD.cur.execute("""SELECT MAX(idReservation) FROM Reservation""")
+    f = BDD.cur.fetchone()[0]
+    if (f==None):
+      idReservation = 1
+    else:
+      idReservation =f+1
+    BDD.cur.execute("""INSERT INTO Reservation(
+                 idReservation, idJeu, idAdherent, idExtension, dateReservation, dureeEmpruntPrevue)
+                VALUES(?, ?, ?, ?, ?, ?)""",
+                (idReservation, None, idAdherent, idExtension, dateReservation, dureeEmpruntPrevue)) 
     BDD.conn.commit()
   
   @staticmethod
-  def annulerReservApresDateButoire(idReservation):
-    Jeu.ajoutExemplaire(Reservation.getIdJeuReserv(idReservation))
-    Adherent.ajoutReservAnnule(Reservation.getIdAdherent(idReservation))
+  def supprimerReserv(idReservation):
     BDD.cur.execute("""DELETE FROM Reservation WHERE idReservation = ?""",
                       (idReservation,))
-    BDD.conn.commit()
+    BDD.cur.commit()
+  
+  @staticmethod
+  def annulerReserv(idReservation):
+    Adherent.ajoutReservAnnule(Reservation.getIdAdhReserv(idReservation))     #on incrément son nombre d'annulation
+    Adherent.setReservEnCours(Reservation.getIdAdhReserv(idReservation), None) #on annule l'id Reservation chez l'adhérent
+    Reservation.supprimerReserv(idReservation)
+    
+  @staticmethod
+  def validerEmprunt(idReservation, date):
+    Adherent.setReservEnCours(Reservation.getIdAdhReserv(idReservation), None) #on annule l'id Reservation chez l'adhérent
+    if ((Reservation.aUnJeu(idReservation))and(Reservation.aUneExtension(idReservation))):  #si l'adherent a réservé 1 jeu + 1 extension
+        Emprunt.emprunterExtensionAvecJeu(Reservation.getIdAdhReserv(idReservation), 
+                                          Reservation.getIdExtensionReserv(idReservation),
+                                         date, Reservation.getDureeEmpruntPrevue(idReservation))
+    elif (Reservation.aUnJeu(idReservation)):                           #si l'adherent a réservé juste 1 jeu
+        Emprunt.emprunterJeu(Reservation.getIdAdhReserv(idReservation), 
+                           Reservation.getIdJeuReserv(idReservation),
+                           date, Reservation.getDureeEmpruntPrevue(idReservation))
+    else:                                                              #si l'adherent a réservé juste 1 extension
+        Emprunt.emprunterExtensionSansJeu(Reservation.getIdAdhReserv(idReservation), 
+                                          Reservation.getIdExtensionReserv(idReservation),
+                                         date, Reservation.getDureeEmpruntPrevue(idReservation))
+    Reservation.supprimerReserv(idReservation)
   
   @staticmethod 
   def enAttente(idReservation):
